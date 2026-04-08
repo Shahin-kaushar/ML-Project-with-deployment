e70fcc6a0165ff45d266234b28f1898bbcda4ec2
Shahin-kaushar <139803353+Shahin-kaushar@users.noreply.github.com>
2026-04-08 19:49:08 +0530

    updated setup.py and fixes

--- application.py
+++ application.py
@@ -11,11 +11,11 @@ application = Flask(__name__)
 app = application
 
 # Route for home page
-@app.route('/')
-def infex():
-    return render_template('index.html')
+# @app.route('/')
+# def infex():
+#     return render_template('index.html')
 
-@app.route('/predictdata', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
@@ -40,4 +40,4 @@ def predict_datapoint():
         return render_template('home.html', results=results[0])
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",Debug=True)
--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ def get_requirements(file_path:str) -> List[str]:
 setup(
 name="mlproject",
 version='0.0.1',
-author="Hemanth Joseph Raj",
-author_email="hemanth1@umd.edu",
+author="Shahin Kaushar",
+author_email="shahinkaushar126@gmail.com",
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 )
